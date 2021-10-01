3770ab03f04b03088f0f5ad697f81feed0e4a5e6
Matias Bermudez <45242740+MatiasAB@users.noreply.github.com>
2021-10-01 12:17:08 -0700

    Adds test for login feature, stem for login module

--- automationpractice/src/test/java/pageObjects/AutomationLoginPage.java
+++ automationpractice/src/test/java/pageObjects/AutomationLoginPage.java
@@ -1,13 +1,72 @@
 package pageObjects;
 
+import java.util.ArrayList;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import frameworks.PageObject;
 
+import foundations.TextBoxControlExtension;
+
 public class AutomationLoginPage extends PageObject {
+	
+	private final String emailId = "email";
+	private final String passId = "passwd";
+	private final String[] ids = {emailId, passId};
+	private final String loginId = "SubmitLogin";
+	private final String registerId = "SubmitCreate";
+	private final String[] buttonIds = {loginId, registerId};
+	private ArrayList<TextBoxControlExtension> loginForm = new ArrayList<TextBoxControlExtension>();
 
 	protected AutomationLoginPage(WebDriver driver, String URL) {
 		super(driver, URL);
+		
+		for (String id: ids) {
+			WebElement element = driver.findElement(By.xpath("//input[@id='"+id+"']"));
+			loginForm.add(new TextBoxControlExtension(element));
+		}
+	}
+	
+	public AutomationProfilePage login() {
+		clickLogin();
+		
+		return new AutomationProfilePage(driver, URL);
+	}
+	
+	private void clickLogin() {
+		WebElement button = getButton(buttonIds[0]);
+		
+		button.click();
+	}
+
+	private WebElement getButton(String id) {
+		return driver.findElement(By.xpath("//button[@id='"+id+"']"));
+	}
+
+	public void enterEmail(String val) {
+		enterField(0, val);
+	}
+	
+	public void enterPass(String val) {
+		enterField(1, val);
+	}
+	
+	public void enterField(int index, String val) {
+		loginForm.get(index).setValue(val);
+	}
+	
+	public String getEmailField() {
+		return getField(0);
+	}
+	
+	public String getPassField() {
+		return getField(1);
+	}
+	
+	public String getField(int index) {
+		return loginForm.get(index).getValue();
 	}
 
 }
--- automationpractice/src/test/java/tests/LoginTest.java
+++ automationpractice/src/test/java/tests/LoginTest.java
@@ -6,14 +6,33 @@ import org.junit.Test;
 
 import foundations.AutomationPracticeTestBase;
 import pageObjects.AutomationHomePage;
+import pageObjects.AutomationLoginPage;
+import pageObjects.AutomationProfilePage;
+import foundations.LoginModule;
 
 public class LoginTest extends AutomationPracticeTestBase {
 
 	@Test
 	public void canLogin() {
-		String expectedURL = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
+		final String expectedURL = "http://automationpractice.com/index.php?controller=my-account";
+		final String email = LoginModule.getEmail();
+		final String pass = LoginModule.getPass();
 		
-		AutomationHomePage page = new AutomationHomePage(getDriver(), this.baseURL);
+		AutomationLoginPage page = new AutomationHomePage(getDriver(), this.baseURL).clickLogin();
+		
+		page.enterEmail(email);
+		
+		assertEquals(email, page.getEmailField());
+		
+		page.enterPass(pass);
+		
+		assertEquals(pass, page.getPassField());
+		
+		AutomationProfilePage page2 = page.login();
+		
+		String currentURL = getDriver().getCurrentUrl();
+		
+		assertEquals(expectedURL, currentURL);
 	}
 
 }
