55f0ac290c8c7eea15fcbd8f83a00f89b537136c
samuel wynsma <cpcs.samuel.wynsma@gmail.com>
2021-09-30 17:56:42 -0700

    Added basic pages and test base.

---
+++ automationpractice/src/test/java/foundations/AutomationPracticeTestBase.java
@@ -0,0 +1,10 @@
+package foundations;
+
+import frameworks.TestBase;
+
+public class AutomationPracticeTestBase extends TestBase {
+
+	protected AutomationPracticeTestBase() {
+		super("http://automationpractice.com/index.php");
+	}
+}
---
+++ automationpractice/src/test/java/pageObjects/AutomationShoppingCartSummary.java
@@ -0,0 +1,12 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+import frameworks.PageObject;
+
+public class AutomationShoppingCartSummary extends PageObject {
+
+	protected AutomationShoppingCartSummary(WebDriver driver, String URL) {
+		super(driver, URL);
+	}
+}
