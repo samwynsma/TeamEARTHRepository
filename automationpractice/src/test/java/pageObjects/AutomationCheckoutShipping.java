e8540ea6d0aea7e56710a0ddefb0f4c75e4b2fdb
Matias Bermudez <45242740+MatiasAB@users.noreply.github.com>
2021-10-12 16:58:31 -0700

    Adds tests for EAR-12

---
+++ automationpractice/src/test/java/pageObjects/AutomationCheckoutShipping.java
@@ -0,0 +1,12 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+import frameworks.PageObject;
+
+public class AutomationCheckoutShipping extends PageObject {
+
+	protected AutomationCheckoutShipping(WebDriver driver, String URL) {
+		super(driver, URL);
+	}
+}
--- automationpractice/src/test/java/tests/ProceedToCheckoutTests.java
+++ automationpractice/src/test/java/tests/ProceedToCheckoutTests.java
@@ -37,4 +37,61 @@ public class ProceedToCheckoutTests extends AutomationPracticeTestBase {
 		
 		assertEquals(expected, actual);
 	}
+	
+	@Test
+	public void canLoginForCheckout() {
+		final String email = LoginModule.getEmail();
+		final String pass = LoginModule.getPass();
+		String expectedURL = "http://automationpractice.com/index.php?controller=order&step=1&multi-shipping=0";
+		
+		AutomationProductListingPage page = new AutomationHomePage(getDriver(), this.baseURL).clickDressesButton();
+		
+		AutomationShoppingCartSummary page2 = page.addPrintedDressToCart();
+		
+		page2.clickProceedButton();
+		
+		AutomationCheckoutLogin page3 = new AutomationCheckoutLogin(getDriver(), this.baseURL);
+		
+		page3.enterEmail(email);
+		
+		assertEquals(email, page3.getEmailField());
+		
+		page3.enterPass(pass);
+		
+		assertEquals(pass, page3.getPassField());
+		
+		AutomationShoppingCartAddress page4 = page3.checkoutLogin();
+		
+		String currentURL = getDriver().getCurrentUrl();
+		
+		assertEquals(expectedURL, currentURL);
+	}
+	
+	@Test
+	public void needsLoginForCheckout() {
+		final String email = LoginModule.getEmail();
+		final String[] expected = {"An email address required.", "Password is required."};
+		
+		AutomationProductListingPage page = new AutomationHomePage(getDriver(), this.baseURL).clickDressesButton();
+		
+		AutomationShoppingCartSummary page2 = page.addPrintedDressToCart();
+		
+		page2.clickProceedButton();
+		
+		AutomationCheckoutLogin page3 = new AutomationCheckoutLogin(getDriver(), this.baseURL);
+		
+		page3.tryLogin();
+		
+		assertEquals(expected[0], page3.readLoginError());
+		
+		page3.findLoginForm();
+		
+		page3.enterEmail(email);
+		
+		assertEquals(email, page3.getEmailField());
+		
+		page3.tryLogin();
+		
+		assertEquals(expected[1], page3.readLoginError());
+	}
 }
